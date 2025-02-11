7d91b4ae5df81521af21a5a6b4b7617a95471f2f
Said-Ait-Driss <saidaitdrissofficial@gmail.com>
2025-02-11 11:30:30 +0100

    fix

--- src/store/slices/taskSlice.ts
+++ src/store/slices/taskSlice.ts
@@ -1,16 +1,26 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit'
 
-interface TaskState {
+interface TasksState {
+  tasks: TaskItem[]
+}
+
+interface TaskItem {
   label: string
   description: string
 }
 
-const initialState: TaskState = { label: '', description: '' }
+const initialState: TasksState = { tasks: [] }
 
 const taskSlice = createSlice({
   name: 'task',
   initialState,
-  reducers: {}
+  reducers: {
+    // getTasks:(state,action:PayloadAction<TasksState[]>){
+    //   const appPlanLimitations = queryClient.getQueryData<AppLimits>([
+    //     'app-limits',
+    //   ]);
+    // }
+  }
 })
 
 // export const { createTask, deleteTask, getTasks } = taskSlice.actions
