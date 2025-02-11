d62d03a28a5de0ab98582f973e59e018411e5f6e
Said-Ait-Driss <saidaitdrissofficial@gmail.com>
2025-02-11 11:15:01 +0100

    feat: react query

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -3,11 +3,16 @@
 import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore } from './index'
+import ReactQueryProvider from './queryClientProvider'
 
 const store = makeStore()
 
 const ReduxProvider = ({ children }: { children: ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <ReactQueryProvider>{children}</ReactQueryProvider>
+    </Provider>
+  )
 }
 
 export default ReduxProvider
