32cf6ebcc69b04708c3e8d1abeb2ea823ce30f18
Said-Ait-Driss <saidaitdrissofficial@gmail.com>
2025-02-12 09:59:49 +0100

    fix: operation & site tables

--- src/views/operation/List.tsx
+++ src/views/operation/List.tsx
@@ -1,10 +1,98 @@
 import type { SystemMode } from '@core/types'
-import Table from '@core/components/mui/Table'
 import Typography from '@mui/material/Typography'
-import { Skeleton } from '@mui/material'
 import { useGetOperationsQuery } from '@/store/features/operation/operationApi'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { ChangeEvent, useState } from 'react'
+import { escapeRegExp } from '@mui/x-data-grid/internals'
+import { formatDateFR } from '@/@core/utils/format'
+import { Chip, Skeleton } from '@mui/material'
+
+interface CellType {
+  row: any
+}
+const columns = (): GridColDef[] => {
+  return [
+    {
+      flex: 1,
+      minWidth: 180,
+      field: 'label',
+      headerName: 'Label',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 500, color: 'text.secondary' }} title={row.label}>
+          {row.label}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 250,
+      field: 'description',
+      headerName: 'Description',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 500, color: 'text.secondary' }} title={row.description}>
+          {row.description}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 250,
+      field: 'tasks',
+      headerName: 'Taches',
+      renderCell: ({ row }: CellType) => (
+        <div className='text-center' title='Taches'>
+          {row.tasks?.map((task: ITask) => <Chip label={task.label} color='info' variant='outlined' />)}
+        </div>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 170,
+      field: 'createdAt',
+      headerName: 'Creation R',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 500, color: 'text.secondary' }}>
+          {formatDateFR(new Date(row.createdAt))}
+        </Typography>
+      )
+    },
+
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      sortable: false
+      // renderCell: ({ row }: CellType) => (
+      //   <RowOptions toggleEditMode={toggleEditMode} deleteObject={deleteObject} row={row} setIsPDFLoading={setIsPDFLoading} />
+      // )
+    }
+  ]
+}
 
 const OperationList = ({ mode }: { mode: SystemMode }) => {
+  const [searchText, setSearchText] = useState<string>('')
+  const [filteredData, setFilteredData] = useState<IRequirement[]>([])
+  const [isFiltering, setIsFiltering] = useState(false)
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
+
+  const handleSearch = (searchValue: string) => {
+    setSearchText(searchValue)
+    const searchRegex = new RegExp(escapeRegExp(searchValue), 'i')
+    const filteredRows = data.filter((row: IRequirement) => {
+      return Object.keys(row).some(field => {
+        if (row[field as keyof IRequirement] !== null && row[field as keyof IRequirement] !== undefined) {
+          return searchRegex.test(row[field as keyof IRequirement]!.toString())
+        }
+      })
+    })
+    if (searchValue.length) {
+      setIsFiltering(true)
+      setFilteredData(filteredRows)
+    } else {
+      setIsFiltering(false)
+      setFilteredData([])
+    }
+  }
   const { data, error, isLoading } = useGetOperationsQuery()
 
   if (error) {
@@ -30,7 +118,34 @@ const OperationList = ({ mode }: { mode: SystemMode }) => {
       <Typography variant='h2' className='my-2'>
         Operation List
       </Typography>
-      <Table data={data} />
+      <DataGrid
+        rowHeight={62}
+        loading={isLoading}
+        rows={data}
+        localeText={{ noRowsLabel: 'Aucune donnes a afficher' }}
+        columns={columns()}
+        // slots={{ toolbar: QuickSearchToolbar }}
+        slotProps={{
+          baseButton: {
+            size: 'medium',
+            variant: 'outlined'
+          },
+          toolbar: {
+            defaultValue: searchText,
+            onChange: (event: ChangeEvent<HTMLInputElement>) => handleSearch(event.target.value),
+            // toggleForm,
+            title: 'Tasks'
+            // handleDateFilter,
+            // clearDateFilter,
+            // data: dataToExport(),
+            // showExcel: true
+          }
+        }}
+        disableRowSelectionOnClick
+        pageSizeOptions={[10, 25, 50]}
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
+      />
     </div>
   )
 }
--- src/views/site/List.tsx
+++ src/views/site/List.tsx
@@ -1,10 +1,101 @@
 import type { SystemMode } from '@core/types'
-import Table from '@core/components/mui/Table'
 import Typography from '@mui/material/Typography'
-import { Skeleton } from '@mui/material'
 import { useGetSiteQuery } from '@/store/features/site/siteApi'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { ChangeEvent, useState } from 'react'
+import { escapeRegExp } from '@mui/x-data-grid/internals'
+import { formatDateFR } from '@/@core/utils/format'
+import { Chip, Skeleton } from '@mui/material'
+
+interface CellType {
+  row: any
+}
+const columns = (): GridColDef[] => {
+  return [
+    {
+      flex: 1,
+      minWidth: 180,
+      field: 'label',
+      headerName: 'Label',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 500, color: 'text.secondary' }} title={row.label}>
+          {row.label}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 250,
+      field: 'description',
+      headerName: 'Description',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 500, color: 'text.secondary' }} title={row.description}>
+          {row.description}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 250,
+      field: 'requirements',
+      headerName: 'Requirements',
+      renderCell: ({ row }: CellType) => (
+        <div className='text-center' title='Requirements'>
+          {row.requirements?.map((requirement: IRequirement) => (
+            <Chip label={requirement.label} color='info' variant='outlined' />
+          ))}
+        </div>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 170,
+      field: 'createdAt',
+      headerName: 'Creation R',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 500, color: 'text.secondary' }}>
+          {formatDateFR(new Date(row.createdAt))}
+        </Typography>
+      )
+    },
+
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      sortable: false
+      // renderCell: ({ row }: CellType) => (
+      //   <RowOptions toggleEditMode={toggleEditMode} deleteObject={deleteObject} row={row} setIsPDFLoading={setIsPDFLoading} />
+      // )
+    }
+  ]
+}
 
 const SiteList = ({ mode }: { mode: SystemMode }) => {
+  const [searchText, setSearchText] = useState<string>('')
+  const [filteredData, setFilteredData] = useState<IRequirement[]>([])
+  const [isFiltering, setIsFiltering] = useState(false)
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
+
+  const handleSearch = (searchValue: string) => {
+    setSearchText(searchValue)
+    const searchRegex = new RegExp(escapeRegExp(searchValue), 'i')
+    const filteredRows = data.filter((row: IRequirement) => {
+      return Object.keys(row).some(field => {
+        if (row[field as keyof IRequirement] !== null && row[field as keyof IRequirement] !== undefined) {
+          return searchRegex.test(row[field as keyof IRequirement]!.toString())
+        }
+      })
+    })
+    if (searchValue.length) {
+      setIsFiltering(true)
+      setFilteredData(filteredRows)
+    } else {
+      setIsFiltering(false)
+      setFilteredData([])
+    }
+  }
+
   const { data, error, isLoading } = useGetSiteQuery()
 
   if (error) {
@@ -29,7 +120,34 @@ const SiteList = ({ mode }: { mode: SystemMode }) => {
       <Typography variant='h2' className='my-2'>
         Site List
       </Typography>
-      <Table data={data} />
+      <DataGrid
+        rowHeight={62}
+        loading={isLoading}
+        rows={data}
+        localeText={{ noRowsLabel: 'Aucune donnes a afficher' }}
+        columns={columns()}
+        // slots={{ toolbar: QuickSearchToolbar }}
+        slotProps={{
+          baseButton: {
+            size: 'medium',
+            variant: 'outlined'
+          },
+          toolbar: {
+            defaultValue: searchText,
+            onChange: (event: ChangeEvent<HTMLInputElement>) => handleSearch(event.target.value),
+            // toggleForm,
+            title: 'Tasks'
+            // handleDateFilter,
+            // clearDateFilter,
+            // data: dataToExport(),
+            // showExcel: true
+          }
+        }}
+        disableRowSelectionOnClick
+        pageSizeOptions={[10, 25, 50]}
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
+      />
     </div>
   )
 }
